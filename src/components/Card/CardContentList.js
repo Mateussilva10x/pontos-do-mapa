4d03d0145736bb9bcf9f67d75acddaa4feea2adc
Mateus Silva <mateussilva651@gmail.com>
2022-05-30 08:45:37 -0300

    teste para clicar no card

--- src/components/Card/CardContentList.js
+++ src/components/Card/CardContentList.js
@@ -4,6 +4,20 @@ import { useMarker } from "../../context/MarkerContext";
 
 const CardContentList = () => {
   const { state } = useMarker();
+  // const google = window.google;
+
+  // console.log(google);
+
+  // const handleClickCard = () => {
+  //   console.log(state);
+
+  //   state.id.setAnimation(google.maps.Animation.BOUNCE, 1000);
+  // };
+
+  // const handleClick = (id) => {
+  //   dispatch({ type: "TESTE", payload: id });
+  // };
+
   return (
     <>
       {state.map((marker, index) => (
@@ -12,6 +26,7 @@ const CardContentList = () => {
           className={
             marker.draggable === true ? "textContent selected" : "textContent"
           }
+          // onClick={() => handleClick(marker.id)}
         >
           <h3>
             <img src={pointIcon} alt="" />
